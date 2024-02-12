6048da22c115dfa73778aaed20dd86fcf035ee57
zahid072 <jahidhasan19368@gmail.com>
2024-02-13 00:07:08 +0600

    rectangle calculation add

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// triangle calculation
+
 function triangleCalculate() {
   const triangleBase = document.getElementById("triangleBase");
   const base = triangleBase.value;
@@ -10,10 +12,39 @@ function triangleCalculate() {
   if (triangleBase.value > 0 && triangleHeight.value > 0) {
     if (base && height) {
       result.innerHTML = `Triangle-Area: ${area} <span>cm<sup>2</sup></span>`;
+    } else {
+      result.innerHTML = "";
     }
+  } else {
+    result.innerHTML = "";
   }
 
   // clear the input
-  document.getElementById("triangleBase").value = "";
-  document.getElementById("triangleHeight").value = "";
+  triangleBase.value = "";
+  triangleHeight.value = "";
+}
+
+// rectangle calculation
+function rectangleCalculation() {
+  const rectangleWidth = document.getElementById("rectangleWidth");
+  const width = rectangleWidth.value;
+  const rectangleLength = document.getElementById("rectangleLength");
+  const length = rectangleLength.value;
+  const result = document.getElementById("result");
+
+  const area = width * length;
+
+  if (rectangleWidth.value > 0 && rectangleLength.value > 0) {
+    if (width && length) {
+      result.innerHTML = `Triangle-Area: ${area} <span>cm<sup>2</sup></span>`;
+    } else {
+      result.innerHTML = "";
+    }
+  } else {
+    result.innerHTML = "";
+  }
+
+  // clear the input
+  rectangleWidth.value = "";
+  rectangleLength.value = "";
 }
